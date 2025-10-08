ce8479e6a122a7a836c3d033490964cbc4137aca
Rahul Sarkar <rahulsarkar2160@gmail.com>
2025-10-09 03:55:22 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -45,7 +45,7 @@ module.exports.isAuthor = async (req, res, next) => {
 
 module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
-    const campground = await review.findById(reviewId);
+    const review = await Review.findById(reviewId);
     if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
