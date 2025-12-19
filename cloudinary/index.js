5433bf60d5361ccd8abe5d57b61f701fd7dfa251
rahulsarkar2160 <rahulsarkar2160@gmail.com>
2025-12-19 21:13:21 +0530

    Fix CloudinaryStorage constructor issue v2

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,21 +1,21 @@
-const cloudinary = require('cloudinary').v2;
-const CloudinaryStorage = require("multer-storage-cloudinary").CloudinaryStorage;
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
+    api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'MemoryMap',
-        allowedFormats: ['jped', 'png', 'jpg']
-    }
+        folder: "MemoryMap",
+        allowed_formats: ["jpeg", "png", "jpg"],
+    },
 });
 
 module.exports = {
     cloudinary,
-    storage
-}
+    storage,
+};
